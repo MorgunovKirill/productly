1750e71c92a410eebedc1f8cc303854526224eb4
Kirill Morgunov <kirillmorgunov@mail.ru>
2022-04-08 10:52:18 +0300

    finished hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,59 @@
 <template>
   <section class="hero">
     <div class="container">
+      <Person v-for="author in authors" :key="author.id" :reply="author.reply" :img="author.img" />
+      <div class="hero__main">
         <h2 class="hero__title">Need a super hero?</h2>
         <p class="hero__description">Do you require some help for your project: Conception workshop,
-        prototyping, marketing strategy, landing page, Ux/UI?</p>
-        <a class="btn btn--bright hero__btn">Contact our expert</a>
+          prototyping, marketing strategy, landing page, Ux/UI?</p>
+        <a href="#" class="btn btn--bright hero__btn">Contact our expert</a>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import Person from "@/components/Person";
+import Avatar from "../assets/img/Avator.png";
+import Avatar1 from "../assets/img/Avator-1.png";
+import Avatar2 from "../assets/img/Avator-2.png";
+import Avatar3 from "../assets/img/Avator-3.png";
+import Avatar4 from "../assets/img/Avator-4.png";
+
 export default {
   name: 'HeroComponent',
+  components: {Person},
+  data() {
+    return {
+      authors: [
+        {
+            id: 1,
+            reply: 'I can help marketing strategy',
+            img: Avatar,
+        },
+        {
+            id: 2,
+            reply: 'I can prototype your app',
+            img: Avatar1,
+        },
+        {
+             id: 3,
+            reply: 'I will define the profile of your users',
+            img: Avatar2,
+        },
+        {
+            id: 4,
+            reply: 'I can design you website',
+            img: Avatar3,
+        },
+        {
+             id: 5,
+            reply: 'I can take care of your pitch',
+            img: Avatar4,
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -19,9 +61,12 @@ export default {
 @import "../assets/sass/vars";
 
 .hero {
+    position: relative;
     background-image: url("../assets/img/Bg-2.svg");
     background-repeat: no-repeat;
     background-position: 0 0;
+    background-size: 100%;
+    padding: 230px 0 379px 0;
 
     &__title {
         font-family: $primary-font;
@@ -31,5 +76,26 @@ export default {
         line-height: 54px;
         margin: 0 0 20px 0;
     }
+
+    &__main {
+      margin: 0 auto;
+      max-width: 542px;
+      text-align: center;
+    }
+
+    &__description {
+      font-family: $primary-font;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 26px;
+      color: $color-text-alt;
+      margin: 0 0 34px 0;
+    }
+
+   &__btn {
+     font-family: $primary-font;
+     font-weight: 700;
+   }
 }
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="person">
+      <p class="person__reply">{{ reply }}</p>
+      <div class="person__img">
+        <img :src="img" alt="author avatar">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonComponent',
+  props: ['reply', 'img']
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/sass/vars";
+
+.person {
+  position: absolute;
+
+  &:nth-child(1) {
+    top: 116px;
+    right: 15%;
+  }
+
+  &:nth-child(2) {
+    top: 439px;
+    right: 10%;
+  }
+
+  &:nth-child(3) {
+    top: 561px;
+    right: 35%;
+  }
+
+  &:nth-child(4) {
+    top: 463px;
+    right: 75%;
+  }
+
+  &:nth-child(5) {
+    top: 181px;
+    right: 70%;
+
+    .person__img {
+      right: initial;
+      top: 60px;
+      left: -50px;
+    }
+
+    .person__reply {
+      &:before {
+        right:  initial;
+        left: 36px;
+      }
+      &:after {
+        right: initial;
+        left: 30px;
+      }
+    }
+  }
+
+  &__reply {
+    position: relative;
+    background: $color-default-white;
+    box-shadow: 0 2px 8px rgba(86, 117, 146, 0.1);
+    border-radius: 50px;
+    padding: 14px 19px 15px 23px;
+    width: fit-content;
+    min-width: 240px;
+    font-family: $primary-font;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: $color-dark-base;
+    z-index: 1;
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: 17px;
+      height: 17px;
+      border-radius: 50%;
+      background-color: $color-default-white;
+      bottom: -6px;
+      right: 36px;
+    }
+
+    &:after {
+      content: "";
+      position: absolute;
+      width: 9px;
+      height: 9px;
+      border-radius: 50%;
+      background-color: $color-default-white;
+      bottom: -17px;
+      right: 30px;
+    }
+  }
+
+  &__img {
+    position: absolute;
+    top: 55px;
+    right: -45px;
+    width: 98px;
+    height: 98px;
+  }
+}
+</style>
